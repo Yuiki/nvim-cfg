b7f84303fe625dd1bbd9c5f8b92175be3765e5ad
Yuiki <10101661+Yuiki@users.noreply.github.com>
2025-06-25 09:08:48 +0900

    first commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.clipboard = "unnamedplus"
+
+require("config.lazy")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
